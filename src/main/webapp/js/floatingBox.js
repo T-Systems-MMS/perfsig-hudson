e551f6cce4453f8d3b3862998ffedc4765dd0db3
Raphael Pionke <Raphael.Pionke@t-systems.com>
2015-10-27 12:45:48 +0100

    small fixes related to JS and jelly files

--- src/main/webapp/js/floatingBox.js
+++ src/main/webapp/js/floatingBox.js
@@ -22,8 +22,10 @@ define(['./common'], function () {
 
                 var json = $.getJSON("performance-signature/getDashboardConfiguration", {testCase: testCase}, function (data) {
                     $.each(data, function (index) {
-                        $(".carousel-inner", carousel).append('<div class="item">' +
-                            data[index].html.replace("###", testCase).replace("./", "performance-signature/") + '</div>\n');
+                        if (data[index].show) {
+                            $(".carousel-inner", carousel).append('<div class="item">' +
+                                data[index].html.replace("###", testCase).replace("./", "performance-signature/") + '</div>\n');
+                        }
                     });
                     $(".carousel-inner div:first-child", carousel).addClass("active");
                 });
